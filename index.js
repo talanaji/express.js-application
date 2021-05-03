7e87fb50653fcd56aa67e0f8bb1f66b3c2a38988
talanaji <talanaji@gmail.com>
2021-05-03 04:06:56 +0200

    use app.route

--- index.js
+++ index.js
@@ -3,19 +3,28 @@ import data from "./data/data.json";
 const app = express();
 const PORT = 3000;
 
-app.use(
-    express.static('public')
-);
+app.use(express.static("public"));
 
-app.use('/images',
-    express.static('images')
-);
+app.use("/images", express.static("images"));
 
-app.get('/', (req, res) => {
-    //res.send(`a get request with / route on port ${PORT}`);
-    res.json(data);
+app.get("/", (req, res) => {
+  //res.send(`a get request with / route on port ${PORT}`);
+  res.json(data);
 });
-app.get('/item/:id', (req, res,next) => {
+app
+  .route("/item")
+  .get((req, res) => {
+    res.send(`a get request with /item route on port ${PORT}`);
+  })
+  .put((req, res) => {
+    res.send(`a put request with /newItem route on port ${PORT}`);
+  })
+  .delete((req, res) => {
+    res.send(`a delete request with /item route on port ${PORT}`);
+  });
+app.get(
+  "/item/:id",
+  (req, res, next) => {
     //res.send(`a get request with / route on port ${PORT}`);
     console.log(req.params.id);
     let user = Number(req.params.id);
@@ -23,20 +32,14 @@ app.get('/item/:id', (req, res,next) => {
     console.log(data[user]);
     res.send(data[user]);
     next();
-},(req,res)=>console.log('did you get the right data'));
-app.post('/newItem', (req, res) => {
-    res.send(`a post request with /newItem route on port ${PORT}`)
-}
+  },
+  (req, res) => console.log("did you get the right data")
 );
-app.put('/item', (req, res) => {
-    res.send(`a put request with /item route on port ${PORT}`)
-}
-);
-app.delete('/item', (req, res) => {
-    res.send(`a delete request with /item route on port ${PORT}`)
-}
-);
+app.post("/newItem", (req, res) => {
+  res.send(`a post request with /newItem route on port ${PORT}`);
+});
+
 app.listen(PORT, () => {
-    console.log(`Your server running on port ${PORT}`);
-    console.log(data);
+  console.log(`Your server running on port ${PORT}`);
+  console.log(data);
 });
